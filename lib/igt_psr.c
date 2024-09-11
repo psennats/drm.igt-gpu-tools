78a4de3672d053fcbf47a336124c8e58a50dd644
Bhanuprakash Modem <bhanuprakash.modem@intel.com>
2024-09-11 13:27:45 +0530

    Revert "lib/igt_psr: Fix PR state check for DisplayPort connectors"
    Causing regression in CI BAT.
    
    This reverts commit 196c2cc9c945a66bb542fdefcf491ea62c1f885e.
    
    Issue: https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/2748
    Cc: Naladala Ramanaidu <ramanaidu.naladala@intel.com>
    Signed-off-by: Bhanuprakash Modem <bhanuprakash.modem@intel.com>
    Acked-by: Naladala Ramanaidu <ramanaidu.naladala@intel.com>

--- lib/igt_psr.c
+++ lib/igt_psr.c
@@ -79,9 +79,8 @@ static bool psr_active_check(int debugfs_fd, enum psr_mode mode, igt_output_t *o
 		c = output->config.connector;
 	}
 
-	if (c->connector_type == DRM_MODE_CONNECTOR_DisplayPort)
-		state = "SRDENT_ON";
-	else if (mode == PR_MODE || mode == PR_MODE_SEL_FETCH)
+	if ((mode == PR_MODE || mode == PR_MODE_SEL_FETCH) &&
+	    c && c->connector_type == DRM_MODE_CONNECTOR_eDP)
 		state = "SLEEP";
 	else if (mode == PSR_MODE_1 || mode == PR_MODE || mode == PR_MODE_SEL_FETCH)
 		state = "SRDENT";
