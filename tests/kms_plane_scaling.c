68c64e4ace7b8b4277171e8399125863b5af7555
Naladala Ramanaidu <ramanaidu.naladala@intel.com>
2024-10-15 19:26:53 +0530

    tests/kms_plane_scaling: Add mode override in scaling test
    Add a call to igt_output_override_mode to ensure the correct mode
    is set for each connector. Modify check_scaling_pipe_plane_rot
    function to include igt_output_override_mode. This change helps
    in accurately testing the scaling functionality by explicitly
    setting the mode before running the tests.
    
    Fixes: 27926dd5792a ("tests/kms_plane_scaling: Find display mode fitting in BW for rotations")
    Signed-off-by: Naladala Ramanaidu <ramanaidu.naladala@intel.com>
    Reviewed-by: Kunal Joshi <kunal1.joshi@intel.com>

--- tests/kms_plane_scaling.c
+++ tests/kms_plane_scaling.c
@@ -585,6 +585,7 @@ check_scaling_pipe_plane_rot(data_t *d, igt_plane_t *plane,
 
 	for_each_connector_mode(output) {
 		mode = &output->config.connector->modes[j__];
+		igt_output_override_mode(output, mode);
 		igt_debug("Trying mode %dx%d\n",
 			  mode->hdisplay, mode->vdisplay);
 
