2b351eca2f7f80557c207cbf0aec94f72ced358c
Lucas De Marchi <lucas.demarchi@intel.com>
2024-11-07 21:43:50 -0800

    tests/intel/xe_drm_fdinfo: Increase sampling period
    This reverts commit 4f79f059766c ("tests/intel/xe_drm_fdinfo: Half the
    execution time") and goes in the opposite direction, doubling it.
    Although in some machines we pass consistently, others like LNL
    show sporadic failures.
    
    Even after changes in the kernel to improve it, it still shows those
    failures. Also, the failures can be much more frequent by stressing all
    the CPUs. Raise the period to reduce the error introduced due to
    additional scheduling delays. This shows a good improvement on LNL that
    doesn't fail anymore without CPU load and fails once every ~200
    iterations with 100% CPU load (vs once every ~3 iterations).
    
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Reviewed-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Link: https://lore.kernel.org/r/20241108054350.3484532-1-lucas.demarchi@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- tests/intel/xe_drm_fdinfo.c
+++ tests/intel/xe_drm_fdinfo.c
@@ -82,7 +82,7 @@ struct pceu_cycles {
 	uint64_t total_cycles;
 };
 
-const unsigned long batch_duration_usec = (1 * USEC_PER_SEC) / 4;
+const unsigned long batch_duration_usec = (1 * USEC_PER_SEC);
 
 static const char *engine_map[] = {
 	"rcs",
