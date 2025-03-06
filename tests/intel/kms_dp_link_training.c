40b32b4346fcd6af2193908f074f62242ae67800
Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>
2025-03-06 16:25:52 +0100

    tests/intel/kms_dp_link_training: Add missing DRIVER_XE
    Commit d7bccece44dd ("tests/intel/kms_dp_link_training: Restrict to Intel
    GPUs") missed adding DRIVER_XE. Ensure both DRIVER_INTEL and DRIVER_XE
    are set to properly restrict the test to Intel GPUs.
    
    Signed-off-by: Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>
    Cc: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Cc: Kunal Joshi <kunal1.joshi@intel.com>
    Cc: Arun R Murthy <arun.r.murthy@intel.com>
    Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

--- tests/intel/kms_dp_link_training.c
+++ tests/intel/kms_dp_link_training.c
@@ -335,7 +335,7 @@ igt_main
 	data_t data = {};
 
 	igt_fixture {
-		data.drm_fd = drm_open_driver_master(DRIVER_INTEL);
+		data.drm_fd = drm_open_driver_master(DRIVER_INTEL | DRIVER_XE);
 		data.devid = intel_get_drm_devid(data.drm_fd);
 		kmstest_set_vt_graphics_mode();
 		igt_display_require(&data.display, data.drm_fd);
