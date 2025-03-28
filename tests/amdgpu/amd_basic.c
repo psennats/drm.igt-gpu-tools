9a36bdee43cb56b7a26ad626c8667489b87199f2
Sunil Khatri <sunil.khatri@amd.com>
2025-03-28 13:54:04 +0530

    tests/amdgpu: Add UMQ submission tests for gfx and compute
    We already have the gfx and compute ip user mode submission
    support added in the IGT and hence with this patch we are
    adding a new test case for gfx and compute using UMQ
    submission.
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Vitaly Prosyak <vitaly.prosyak@amd.com>

--- tests/amdgpu/amd_basic.c
+++ tests/amdgpu/amd_basic.c
@@ -747,6 +747,22 @@ igt_main
 		}
 	}
 
+	igt_describe("Check-GFX-CS-for-every-available-ring-works-for-write-const-fill-and-copy-operation-using-more-than-one-IB-and-shared-IB");
+	igt_subtest_with_dynamic("cs-gfx-with-IP-GFX-UMQ") {
+		if (arr_cap[AMD_IP_GFX]) {
+			igt_dynamic_f("cs-gfx-with-umq")
+			amdgpu_command_submission_gfx(device, info.hw_ip_version_major < 11, true);
+		}
+	}
+
+	igt_describe("Check-COMPUTE-CS-for-every-available-ring-works-for-write-const-fill-copy-and-nop-operation");
+	igt_subtest_with_dynamic("cs-compute-with-IP-COMPUTE-UMQ") {
+		if (arr_cap[AMD_IP_COMPUTE]) {
+			igt_dynamic_f("cs-compute-with-umq")
+			amdgpu_command_submission_compute(device, true);
+		}
+	}
+
 	igt_fixture {
 		amdgpu_device_deinitialize(device);
 		drm_close_driver(fd);
