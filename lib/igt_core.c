psennats/drm.igt-gpu-tools

commit 7c1b733d638c24d7ad70e6cd6188488eda65a4b6

lib/igt_core: Add a dummy workaround when libunwind is not present
If HAVE_LIBUNWIND is not defined, print a log message indicating
"backtrace not implemented" in the print_backtrace* functions and
return.

As xputch, xpow, printnum, xstrlcpy, xprintfmt, xprintf are used
by backtrace functions, enabled those only if HAVE_LIBUNWIND is defined.

Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

lib/igt_core.c
@@ -82,7 +82,9 @@
 #include "runnercomms.h"
 
 #define UNW_LOCAL_ONLY
+#ifdef HAVE_LIBUNWIND
 #include <libunwind.h>
+#endif
 #include <elfutils/libdwfl.h>
 
 #ifdef HAVE_LIBGEN_H
@@ -375,6 +377,9 @@ static int _igt_dynamic_tests_executed = -1;
 
 static void print_backtrace(void)
 {
+#ifndef HAVE_LIBUNWIND
+	igt_info("backtrace not implemented\n");
+#else
 	unw_cursor_t cursor;
 	unw_context_t uc;
 	int stack_num = 0;
@@ -432,6 +437,7 @@ static void print_backtrace(void)
 
 	if (dwfl)
 		dwfl_end(dwfl);
+#endif
 }
 
 __attribute__((format(printf, 2, 3)))
@@ -2078,6 +2084,7 @@ static void write_stderr(const char *str)
 	__write_stderr(str, strlen(str));
 }
 
+#ifdef HAVE_LIBUNWIND
 static const char hex[] = "0123456789abcdef";
 
 static void
@@ -2230,9 +2237,13 @@ xprintf(const char *fmt, ...)
 	xprintfmt(fmt, ap);
 	va_end(ap);
 }
+#endif /* HAVE_LIBUNWIND */
 
 static void print_backtrace_sig_safe(void)
 {
+#ifndef HAVE_LIBUNWIND
+	igt_info("backtrace not implemented\n");
+#else
 	unw_cursor_t cursor;
 	unw_context_t uc;
 	int stack_num = 0;
@@ -2252,6 +2263,7 @@ static void print_backtrace_sig_safe(void)
 				(unsigned int) off);
 
 	}
+#endif
 }
 
 void __igt_fail_assert(const char *domain, const char *file, const int line,

meson.build
@@ -147,6 +147,9 @@ else
 endif
 
 libunwind = dependency('libunwind', required : get_option('libunwind'))
+if libunwind.found()
+	config.set('HAVE_LIBUNWIND', 1)
+endif
 build_info += 'With libunwind: @0@'.format(libunwind.found())
 
 libdw = dependency('libdw', required : true)

commit cfb183137f2e31b429d9565d7ebd5c0580380609

lib/igt_core: Fix printing when libunwind is not present
Pipeline debian-minimal is failing with:

igt_thread: ../lib/tests/igt_thread.c:143: main: Assertion `matches(out, "\\[thread:.*\\] Stack trace")' failed.

Test lib/tests/igt_thread expects the test failure accompanied by
a stack trace after which dump should follow.

Fix the message so this lib test should work.

Fixes: 7c1b733d638c ("lib/igt_core: Add a dummy workaround when libunwind is not present")
Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
Reviewed-by: Sk Anirban <sk.anirban@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

lib/igt_core.c
@@ -378,7 +378,7 @@ static int _igt_dynamic_tests_executed = -1;
 static void print_backtrace(void)
 {
 #ifndef HAVE_LIBUNWIND
-	igt_info("backtrace not implemented\n");
+	igt_info("Stack trace: not implemented\n");
 #else
 	unw_cursor_t cursor;
 	unw_context_t uc;
@@ -2242,7 +2242,7 @@ xprintf(const char *fmt, ...)
 static void print_backtrace_sig_safe(void)
 {
 #ifndef HAVE_LIBUNWIND
-	igt_info("backtrace not implemented\n");
+	igt_info("Stack trace: not implemented\n");
 #else
 	unw_cursor_t cursor;
 	unw_context_t uc;