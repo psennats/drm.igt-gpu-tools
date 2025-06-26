psennats/drm.igt-gpu-tools

commit 7e75d12ee64a2293ca74167618569e28cccd4123

lib/amdgpu: Generalize command submission NOP function
Rename and refactor amdgpu_command_submission_compute_nop() to be more
generic:
- Renamed to amdgpu_command_submission_nop()
- Added type parameter to specify IP block type (COMPUTE/DMA/etc)
- Updated all related queue create/submit/destroy calls to use the type
- Added include for amd_ip_blocks.h in header
- Updated function prototype in header file

This change makes the NOP submission function reusable across different
IP blocks rather than being compute-specific. The function now takes
the IP block type as a parameter and uses it for queue operations.

Signed-off-by: Jesse Zhang <Jesse.Zhang@amd.com>
Reviewed-by: Vitaly Prosyak <vitaly.prosyak@amd.com>

lib/amdgpu/amd_compute.c
@@ -32,7 +32,7 @@
  * @param device
  * @param user_queue
  */
-void amdgpu_command_submission_compute_nop(amdgpu_device_handle device, bool user_queue)
+void amdgpu_command_submission_nop(amdgpu_device_handle device, enum amd_ip_block_type type, bool user_queue)
 {
 	amdgpu_context_handle context_handle;
 	amdgpu_bo_handle ib_result_handle;
@@ -57,7 +57,7 @@ void amdgpu_command_submission_compute_nop(amdgpu_device_handle device, bool use
 	igt_assert_eq(r, 0);
 
 	if (user_queue) {
-		amdgpu_user_queue_create(device, ring_context, AMD_IP_COMPUTE);
+		amdgpu_user_queue_create(device, ring_context, type);
 	} else {
 		r = amdgpu_cs_ctx_create(device, &context_handle);
 		igt_assert_eq(r, 0);
@@ -90,7 +90,7 @@ void amdgpu_command_submission_compute_nop(amdgpu_device_handle device, bool use
 		ring_context->pm4_dw = 16;
 
 		if (user_queue) {
-			amdgpu_user_queue_submit(device, ring_context, AMD_IP_COMPUTE,
+			amdgpu_user_queue_submit(device, ring_context, type,
 						 ib_result_mc_address);
 		} else {
 			memset(&ib_info, 0, sizeof(struct amdgpu_cs_ib_info));
@@ -128,7 +128,7 @@ void amdgpu_command_submission_compute_nop(amdgpu_device_handle device, bool use
 	}
 
 	if (user_queue) {
-		amdgpu_user_queue_destroy(device, ring_context, AMD_IP_COMPUTE);
+		amdgpu_user_queue_destroy(device, ring_context, type);
 	} else {
 		r = amdgpu_cs_ctx_free(context_handle);
 		igt_assert_eq(r, 0);

lib/amdgpu/amd_compute.h
@@ -24,8 +24,8 @@
  */
 #ifndef AMD_COMPUTE_H
 #define AMD_COMPUTE_H
+#include "amd_ip_blocks.h"
 
-
-void amdgpu_command_submission_compute_nop(amdgpu_device_handle device, bool user_queue);
+void amdgpu_command_submission_nop(amdgpu_device_handle device, enum amd_ip_block_type type,  bool user_queue);
 
 #endif

tests/amdgpu/amd_basic.c
@@ -116,7 +116,7 @@ static void amdgpu_command_submission_compute(amdgpu_device_handle device, bool 
 						     get_ip_block(device, AMDGPU_HW_IP_COMPUTE),
 						     user_queue);
 	/* nop test */
-	amdgpu_command_submission_compute_nop(device, user_queue);
+	amdgpu_command_submission_nop(device, AMDGPU_HW_IP_COMPUTE, user_queue);
 }
 
 /**

commit 6d598a52a104033e8b7eaa2916897c4b256f625e

lib/amdgpu: Generalize command submission to support SDMA
Modify amdgpu_command_submission_nop() to handle both compute and SDMA
command submissions by:
1. Adding SDMA header include
2. Using the passed IP type parameter instead of hardcoding COMPUTE
3. Adding SDMA-specific NOP packet handling
4. Propagating the IP type through fence status and IB requests

Fixes: 7e75d12 (lib/amdgpu: Generalize command submission NOP function)

Signed-off-by: Jesse Zhang <Jesse.Zhang@amd.com>
Reviewed-by: Vitaly Prosyak <vitaly.prosyak@amd.com>

lib/amdgpu/amd_compute.c
@@ -25,6 +25,7 @@
 #include "amd_PM4.h"
 #include "amd_memory.h"
 #include "amd_compute.h"
+#include "amd_sdma.h"
 #include "amd_userq.h"
 
 /**
@@ -53,7 +54,7 @@ void amdgpu_command_submission_nop(amdgpu_device_handle device, enum amd_ip_bloc
 	ring_context = calloc(1, sizeof(*ring_context));
 	igt_assert(ring_context);
 
-	r = amdgpu_query_hw_ip_info(device, AMDGPU_HW_IP_COMPUTE, 0, &info);
+	r = amdgpu_query_hw_ip_info(device, type, 0, &info);
 	igt_assert_eq(r, 0);
 
 	if (user_queue) {
@@ -86,7 +87,10 @@ void amdgpu_command_submission_nop(amdgpu_device_handle device, enum amd_ip_bloc
 
 		ptr = ib_result_cpu;
 		memset(ptr, 0, 16);
-		ptr[0] = PACKET3(PACKET3_NOP, 14);
+		if (type == AMDGPU_HW_IP_DMA)
+			ptr[0] = SDMA_NOP;
+		else
+			ptr[0] = PACKET3(PACKET3_NOP, 14);
 		ring_context->pm4_dw = 16;
 
 		if (user_queue) {
@@ -98,7 +102,7 @@ void amdgpu_command_submission_nop(amdgpu_device_handle device, enum amd_ip_bloc
 			ib_info.size = 16;
 
 			memset(&ibs_request, 0, sizeof(struct amdgpu_cs_request));
-			ibs_request.ip_type = AMDGPU_HW_IP_COMPUTE;
+			ibs_request.ip_type = type;
 			ibs_request.ring = instance;
 			ibs_request.number_of_ibs = 1;
 			ibs_request.ibs = &ib_info;
@@ -110,7 +114,7 @@ void amdgpu_command_submission_nop(amdgpu_device_handle device, enum amd_ip_bloc
 			igt_assert_eq(r, 0);
 
 			fence_status.context = context_handle;
-			fence_status.ip_type = AMDGPU_HW_IP_COMPUTE;
+			fence_status.ip_type = type;
 			fence_status.ip_instance = 0;
 			fence_status.ring = instance;
 			fence_status.fence = ibs_request.seq_no;