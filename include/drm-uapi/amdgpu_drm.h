51fb6cc6a8550c40e4661a6e0ec12af75859901e
Jesse.Zhang <Jesse.Zhang@amd.com>
2025-07-07 14:39:20 +0800

    drm-uapi/amdgpu: Add num_userq_slots to hw_ip_info structure
    Add a new field num_userq_slots to struct drm_amdgpu_info_hw_ip to expose
    the number of available user queue slots per hardware IP block. This
    enables userspace to query and utilize user queues more effectively.
    
    The field will be used by the amdgpu driver to communicate the number
    of available user-mode queue slots to userspace applications
    
    kernel patch link:
    https://lists.freedesktop.org/archives/amd-gfx/2025-July/126981.html
    
    Signed-off-by: Jesse Zhang <Jesse.Zhang@amd.com>
    Reviewed-by: Vitaly Prosyak <vitaly.prosyak@amd.com>

--- include/drm-uapi/amdgpu_drm.h
+++ include/drm-uapi/amdgpu_drm.h
@@ -1465,6 +1465,7 @@ struct drm_amdgpu_info_hw_ip {
 	__u32  available_rings;
 	/** version info: bits 23:16 major, 15:8 minor, 7:0 revision */
 	__u32  ip_discovery_version;
+	__u32  num_userq_slots;
 };
 
 /* GFX metadata BO sizes and alignment info (in bytes) */
