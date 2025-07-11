psennats/drm.igt-gpu-tools

commit 3a39a6e51b186d43b4f6f24b0537b178ff119aa6

lib/xe: Populate vram_size[] and visible_vram_size[] by GT ID
vram_size[] and visible_vram_size[] are used to lookup VRAM information
by uapi GT ID.  Ensure that they're populated according to these IDs
rather than by the GT's index in the query list; on future platforms
these two may not be the same.

v2:
 - s/ffs()/ffsll()/  (Zbigniew)

Cc: Zbigniew Kempczyński <zbigniew.kempczynski@intel.com>
Reviewed-by: Zbigniew Kempczyński <zbigniew.kempczynski@intel.com>
Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

lib/xe/xe_query.c
@@ -332,6 +332,7 @@ static void xe_device_free(struct xe_device *xe_dev)
 struct xe_device *xe_device_get(int fd)
 {
 	struct xe_device *xe_dev, *prev;
+	int max_gt;
 
 	xe_dev = find_in_cache(fd);
 	if (xe_dev)
@@ -355,14 +356,22 @@ struct xe_device *xe_device_get(int fd)
 	xe_dev->engines = xe_query_engines(fd);
 	xe_dev->mem_regions = xe_query_mem_regions_new(fd);
 	xe_dev->oa_units = xe_query_oa_units_new(fd);
-	xe_dev->vram_size = calloc(xe_dev->gt_list->num_gt, sizeof(*xe_dev->vram_size));
-	xe_dev->visible_vram_size = calloc(xe_dev->gt_list->num_gt, sizeof(*xe_dev->visible_vram_size));
-	for (int gt = 0; gt < xe_dev->gt_list->num_gt; gt++) {
-		xe_dev->vram_size[gt] = gt_vram_size(xe_dev->mem_regions,
-						     &xe_dev->gt_list->gt_list[gt]);
-		xe_dev->visible_vram_size[gt] =
-			gt_visible_vram_size(xe_dev->mem_regions,
-					     &xe_dev->gt_list->gt_list[gt]);
+
+	/*
+	 * vram_size[] and visible_vram_size[] are indexed by uapi ID; ensure
+	 * the allocation is large enough to hold the highest GT ID
+	 */
+	max_gt = ffsll(xe_dev->gt_mask) - 1;
+	xe_dev->vram_size = calloc(max_gt, sizeof(*xe_dev->vram_size));
+	xe_dev->visible_vram_size = calloc(max_gt, sizeof(*xe_dev->visible_vram_size));
+
+	for (int idx = 0; idx < xe_dev->gt_list->num_gt; idx++) {
+		struct drm_xe_gt *gt = &xe_dev->gt_list->gt_list[idx];
+
+		xe_dev->vram_size[gt->gt_id] =
+			gt_vram_size(xe_dev->mem_regions, gt);
+		xe_dev->visible_vram_size[gt->gt_id] =
+			gt_visible_vram_size(xe_dev->mem_regions, gt);
 	}
 	xe_dev->default_alignment = __mem_default_alignment(xe_dev->mem_regions);
 	xe_dev->has_vram = __mem_has_vram(xe_dev->mem_regions);

commit 64c0be2da10d441f4054f139d6ff14d2d5c2c3e3

lib/xe: Fix allocation for vram_size and visible_vram_size based on max_gt
The current allocation for vram_size and visible_vram_size has two
issues:

* The expression (max_gt = ffsll(xe_dev->gt_mask) - 1) gives us the
  zero-based index of the first bit set in the mask, which does not give
  us the maximum GT ID, but the minimum.

* Since max_gt is zero-based, even if its value was right, we would
  still be allocating not enough entries for the array.

Fix that by calculating max_gt in the same way that xe_dev_max_gt() does
and using (max_gt + 1) as argument to calloc().

Cc: Matt Roper <matthew.d.roper@intel.com>
Cc: Zbigniew Kempczyński <zbigniew.kempczynski@intel.com>
Fixes: 3a39a6e51b18 ("lib/xe: Populate vram_size[] and visible_vram_size[] by GT ID")
Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

lib/xe/xe_query.c
@@ -361,9 +361,9 @@ struct xe_device *xe_device_get(int fd)
 	 * vram_size[] and visible_vram_size[] are indexed by uapi ID; ensure
 	 * the allocation is large enough to hold the highest GT ID
 	 */
-	max_gt = ffsll(xe_dev->gt_mask) - 1;
-	xe_dev->vram_size = calloc(max_gt, sizeof(*xe_dev->vram_size));
-	xe_dev->visible_vram_size = calloc(max_gt, sizeof(*xe_dev->visible_vram_size));
+	max_gt = igt_fls(xe_dev->gt_mask) - 1;
+	xe_dev->vram_size = calloc(max_gt + 1, sizeof(*xe_dev->vram_size));
+	xe_dev->visible_vram_size = calloc(max_gt + 1, sizeof(*xe_dev->visible_vram_size));
 
 	for (int idx = 0; idx < xe_dev->gt_list->num_gt; idx++) {
 		struct drm_xe_gt *gt = &xe_dev->gt_list->gt_list[idx];