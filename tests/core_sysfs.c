f830df6c730bfb4d5b05e2ba83e1e20e37f4f22a
Peter Senna Tschudin <peter.senna@linux.intel.com>
2025-07-16 11:36:52 +0200

    tests: Add core_sysfs
    core_sysfs is GPU-agnostic test designed to work with any GPU, not
    limited to i915 and Xe. The test scans the sysfs directory associated
    with the GPU, reads all files, and discards the content.
    
    Cc: kamil.konieczny@linux.intel.com
    Cc: katarzyna.piecielska@intel.com
    Cc: zbigniew.kempczynski@intel.com
    Cc: michal.wajdeczko@intel.com
    Cc: karthik.b.s@intel.com
    Cc: ewelina.musial@intel.com
    Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@linux.intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

---
+++ tests/core_sysfs.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2025 Intel Corporation
+ */
+
+#include "igt.h"
+#include "igt_dir.h"
+#include "igt_sysfs.h"
+
+/**
+ * TEST: sysfs test
+ * Description: Read entries from sysfs path.
+ * Category: Core
+ * Mega feature: General Core features
+ * Sub-category: uapi
+ * Functionality: sysfs
+ * Feature: core
+ * Test category: uapi
+ *
+ * SUBTEST: read-all-entries
+ * Description: Read all entries from sysfs path
+ *
+ */
+
+IGT_TEST_DESCRIPTION("Read entries from sysfs paths.");
+
+igt_main
+{
+	int fd = -1;
+	int sysfs = -1;
+
+	igt_fixture {
+		fd = drm_open_driver_master(DRIVER_ANY);
+		sysfs = igt_sysfs_open(fd);
+		igt_require(sysfs >= 0);
+
+		kmstest_set_vt_graphics_mode();
+	}
+
+	igt_describe("Read all entries from sysfs path.");
+	igt_subtest("read-all-entries")
+		igt_dir_process_files_simple(sysfs);
+
+	igt_fixture {
+		close(sysfs);
+		drm_close_driver(fd);
+	}
+}
