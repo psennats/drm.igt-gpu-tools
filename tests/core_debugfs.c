7504a6d46c5652e4af410aa89ee21fd0ba619194
Peter Senna Tschudin <peter.senna@linux.intel.com>
2025-07-16 11:36:50 +0200

    tests: Add core_debugfs
    Introduce core_debugfs that is expected to work with any GPU, not
    limited to i915 and Xe. The test attempts to open every file in debugfs
    associated with the GPU.
    
    Cc: katarzyna.piecielska@intel.com
    Cc: zbigniew.kempczynski@intel.com
    Cc: michal.wajdeczko@intel.com
    Cc: karthik.b.s@intel.com
    Cc: ewelina.musial@intel.com
    Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@linux.intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

---
+++ tests/core_debugfs.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2025 Intel Corporation
+ */
+
+#include "igt.h"
+#include "igt_debugfs.h"
+#include "igt_dir.h"
+
+/**
+ * TEST: debugfs test
+ * Description: Read entries from debugfs
+ * Category: Core
+ * Mega feature: General Core features
+ * Sub-category: uapi
+ * Functionality: debugfs
+ * Feature: core
+ * Test category: uapi
+ *
+ * SUBTEST: read-all-entries
+ * Description: Read all entries from debugfs path validating debugfs entries
+ */
+
+IGT_TEST_DESCRIPTION("Read entries from debugfs");
+
+igt_main
+{
+	int debugfs = -1;
+	int fd = -1;
+
+	igt_fixture {
+		fd = drm_open_driver_master(DRIVER_ANY);
+		debugfs = igt_debugfs_dir(fd);
+		igt_require(debugfs >= 0);
+
+		kmstest_set_vt_graphics_mode();
+	}
+
+	igt_describe("Read all entries from debugfs path.");
+	igt_subtest("read-all-entries") {
+		igt_dir_process_files_simple(debugfs);
+	}
+
+	igt_fixture {
+		close(debugfs);
+		drm_close_driver(fd);
+	}
+}
