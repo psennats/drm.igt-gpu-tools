psennats/drm.igt-gpu-tools

commit 26ddb59c1d5f8d68500f2d09263afe919c79a8d2

lib/pciids: Add one more PCI ID to BMG
Align with kernel commit ccfb15b8158c ("drm/xe/bmg: Add one additional PCI ID")

It adds one more pci id to BMG.

v1:
 - Commit id of merged patch is to be specified under kernel commit (Jani Nikula)
v2:
 - Update with correct commit id of respective kernel patch.
 - update first alphabet of Add with lowercase (Dnyaneshwar)
v3:
 - Updated first letter of Add with lowercase
 - Missed specifying v3 version history in commit message. (Dnyaneshwar)
v4:
 - Added v3 version history in commit message.

Signed-off-by: Ravi Kumar Vodapalli <ravi.kumar.vodapalli@intel.com>
Reviewed-by: Dnyaneshwar Bhadane <dnyaneshwar.bhadane@intel.com>

lib/pciids.h
@@ -846,6 +846,7 @@
 /* BMG */
 #define INTEL_BMG_IDS(MACRO__, ...) \
 	MACRO__(0xE202, ## __VA_ARGS__), \
+	MACRO__(0xE209, ## __VA_ARGS__), \
 	MACRO__(0xE20B, ## __VA_ARGS__), \
 	MACRO__(0xE20C, ## __VA_ARGS__), \
 	MACRO__(0xE20D, ## __VA_ARGS__), \

commit 3896691534efb44b2a5c407eda784dcb0419b6b5

lib/pciids: lib/pciids: Add Wildcat Lake
Syncing lib/pciids.h with kernel include/drm/intel/pciids.h means there
must exist kernel and igt commits where the files are identical,
i.e. the file is copied verbatim at some kernel commit to igt.

It does *not* mean syncing the *changes* of a kernel commit to the igt
version of the file.

The latest changes to lib/pciids.h have gone out of sync with kernel,
completely missing kernel commit 3c0f211bc8fc ("drm/xe: Add Wildcat Lake
device IDs to PTL list").

Properly sync PCI IDs with kernel commit ccfb15b8158c ("drm/xe/bmg: Add
one additional PCI ID"), including the full file.

Fixes: 26ddb59c1d5f ("lib/pciids: Add one more PCI ID to BMG")
Cc: Ravi Kumar Vodapalli <ravi.kumar.vodapalli@intel.com>
Cc: Dnyaneshwar Bhadane <dnyaneshwar.bhadane@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
Signed-off-by: Jani Nikula <jani.nikula@intel.com>

lib/pciids.h
@@ -872,6 +872,8 @@
 	MACRO__(0xB08F, ## __VA_ARGS__), \
 	MACRO__(0xB090, ## __VA_ARGS__), \
 	MACRO__(0xB0A0, ## __VA_ARGS__), \
-	MACRO__(0xB0B0, ## __VA_ARGS__)
+	MACRO__(0xB0B0, ## __VA_ARGS__), \
+	MACRO__(0xFD80, ## __VA_ARGS__), \
+	MACRO__(0xFD81, ## __VA_ARGS__)
 
 #endif /* __PCIIDS_H__ */