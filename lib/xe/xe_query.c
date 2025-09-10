psennats/drm.igt-gpu-tools

commit 3bb0c49ac668cb0818f8afccbe6d2b5d20e2faf3

lib/xe/xe_query: Add xe_gt_type() and xe_is_main_gt() helpers
Introduce xe_gt_type() to return the GT type from query data. This
replaces open-coded access in xe_is_media_gt() and makes it easy to
add new GT-specific helpers. Add xe_is_main_gt() for MAIN GT checks.

Signed-off-by: Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>
Cc: Adam Miszczak <adam.miszczak@linux.intel.com>
Cc: Jakub Kolakowski <jakub1.kolakowski@intel.com>
Cc: Lukasz Laguna <lukasz.laguna@intel.com>
Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
Cc: Satyanarayana K V P <satyanarayana.k.v.p@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

lib/xe/xe_query.c
@@ -917,24 +917,46 @@ bool xe_has_media_gt(int fd)
 }
 
 /**
+ * xe_gt_type:
+ * @fd: xe device fd
+ * @gt: gt id
+ *
+ * Returns the type of @gt for device @fd (e.g.,
+ * DRM_XE_QUERY_GT_TYPE_MAIN, DRM_XE_QUERY_GT_TYPE_MEDIA).
+ */
+uint16_t xe_gt_type(int fd, int gt)
+{
+	struct xe_device *xe_dev = find_in_cache(fd);
+
+	igt_assert(xe_dev);
+	igt_assert_f(gt >= 0 && gt < xe_number_gt(fd),
+		     "gt %d out of range [0..%d)\n", gt, xe_number_gt(fd));
+
+	return xe_dev->gt_list->gt_list[gt].type;
+}
+
+/**
  * xe_is_media_gt:
  * @fd: xe device fd
  * @gt: gt id
  *
- * Returns true if @gt for device @fd is media GT, otherwise false.
+ * Returns true if @gt for device @fd is MEDIA GT, otherwise false.
  */
 bool xe_is_media_gt(int fd, int gt)
 {
-	struct xe_device *xe_dev;
+	return xe_gt_type(fd, gt) == DRM_XE_QUERY_GT_TYPE_MEDIA;
+}
 
-	xe_dev = find_in_cache(fd);
-	igt_assert(xe_dev);
-	igt_assert(gt < xe_number_gt(fd));
-
-	if (xe_dev->gt_list->gt_list[gt].type == DRM_XE_QUERY_GT_TYPE_MEDIA)
-		return true;
-
-	return false;
+/**
+ * xe_is_main_gt:
+ * @fd: xe device fd
+ * @gt: gt id
+ *
+ * Returns true if @gt for device @fd is MAIN GT, otherwise false.
+ */
+bool xe_is_main_gt(int fd, int gt)
+{
+	return xe_gt_type(fd, gt) == DRM_XE_QUERY_GT_TYPE_MAIN;
 }
 
 /**

lib/xe/xe_query.h
@@ -127,7 +127,9 @@ const char *xe_engine_class_short_string(uint32_t engine_class);
 bool xe_has_engine_class(int fd, uint16_t engine_class);
 struct drm_xe_engine *xe_find_engine_by_class(int fd, uint16_t engine_class);
 bool xe_has_media_gt(int fd);
+uint16_t xe_gt_type(int fd, int gt);
 bool xe_is_media_gt(int fd, int gt);
+bool xe_is_main_gt(int fd, int gt);
 uint16_t xe_gt_get_tile_id(int fd, int gt);
 uint32_t *xe_hwconfig_lookup_value(int fd, enum intel_hwconfig attribute, uint32_t *len);
 

commit f41ee6298227803669304f56917e85297ddbc55b

lib/xe/xe_query: Don’t assume sequential GT IDs in xe_gt_type()
Replace direct gt_list indexing with drm_xe_get_gt() and check
gt_mask before use. This ensures xe_gt_type() works with real
GT IDs instead of assuming list index == uapi ID, consistent
with other helpers.

v2: Drop redundant assert on gt_mask (Kamil)

Fixes: 3bb0c49ac668 ("lib/xe/xe_query: Add xe_gt_type() and xe_is_main_gt() helpers")

Signed-off-by: Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
Cc: Adam Miszczak <adam.miszczak@linux.intel.com>
Cc: Jakub Kolakowski <jakub1.kolakowski@intel.com>
Cc: Kamil Konieczny <kamil.konieczny@linux.intel.com>
Cc: Lukasz Laguna <lukasz.laguna@intel.com>
Cc: Piotr Piórkowski <piotr.piorkowski@intel.com>
Cc: Satyanarayana K V P <satyanarayana.k.v.p@intel.com>

lib/xe/xe_query.c
@@ -927,12 +927,13 @@ bool xe_has_media_gt(int fd)
 uint16_t xe_gt_type(int fd, int gt)
 {
 	struct xe_device *xe_dev = find_in_cache(fd);
+	const struct drm_xe_gt *xe_gt;
 
 	igt_assert(xe_dev);
-	igt_assert_f(gt >= 0 && gt < xe_number_gt(fd),
-		     "gt %d out of range [0..%d)\n", gt, xe_number_gt(fd));
+	xe_gt = drm_xe_get_gt(xe_dev, gt);
+	igt_assert(xe_gt);
 
-	return xe_dev->gt_list->gt_list[gt].type;
+	return xe_gt->type;
 }
 
 /**